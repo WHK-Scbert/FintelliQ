134b6354e761de637c3c689a769f3d56a5edf1ce
chanonmark5 <chanonmark5@hotmail.com>
2024-01-14 23:11:56 -0700

    Clean Input

--- app.py
+++ app.py
@@ -29,98 +29,44 @@ def main():
     # Sidebar for user input
     ticker = st.sidebar.text_input("Enter Stock Ticker:", "NIO")
     client = OpenAI()
+    msft = yahooFinance.Ticker(ticker)
 
     assistance_ID = 'asst_vSYVvJ3sGbisAl7wkpANHgwj'
+
+    # Fetch news from Yahoo Finance
+    yahoo_news = ""
+    yahoo_links = msft.news
+    for source in yahoo_links:
+        #print(source['link'])
+        content = requests.get(source['link']).text
+        yahoo_news += function.extract_content(content)
     
-
-
+    st.session_state.general_news = function.gpt_handler(assistance_ID, yahoo_news, client, "general")
+    function.display_assistant_message(st.session_state.general_news)
     
-
-
     if st.sidebar.button("Fetch News"):
+        # Clear the previous messages
+        st.empty()
+
+        # Fetch news from Benzinga
         url = f'https://www.benzinga.com/quote/{ticker}/news'
         http_links = function.scrape_http_links(url)
         content = [requests.get(link).text for link in http_links]
         concatenated_news = function.concatenate_elements(content)
-        #concatenated_news = concatenated_news[:32000]
         processed_text = function.extract_content(concatenated_news)
 
-        # processed_text = function.process_text_with_regex(concatenated_news)
-        # processed_text = function.clean_text(processed_text)
-        # processed_text = processed_text[:32000]
-        print(processed_text)
-
-        thread = client.beta.threads.create()
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=processed_text
-        )
-
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistance_ID,
-            instructions="Sanitize all the unreadable data and put it into bullet points"
-        )
-
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id
-        )
+        # Analyze the news with IntelliQ Assistance
+        st.session_state.messages = function.gpt_handler(assistance_ID, processed_text, client)
         
-        st.session_state.run = run
-        pending = False
-        while st.session_state.run.status != "completed":
-            if not pending:
-                with st.chat_message("assistant"):
-                    st.markdown("Processing...")
-                pending = True
-            time.sleep(3)
-            st.session_state.run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id,
-            )
-            
-                
-        if st.session_state.run.status == "completed": 
-            st.empty()
-        
-            
-        st.session_state.messages = client.beta.threads.messages.list(
-            thread_id=thread.id
-        ).data
-        
-        for message in reversed(st.session_state.messages):
-            if message.role in ["assistant"]:
-                with st.chat_message(message.role):
-                    for content in message.content:
-                        if content.type == "text":
-                            st.markdown(content.text.value)
-                        elif content.type == "image_file":
-                            image_file = content.image_file.file_id
-                            image_data = client.files.content(image_file)
-                            image_data = image_data.read()
-                            #save image to temp file
-                            temp_file = tempfile.NamedTemporaryFile(delete=False)
-                            temp_file.write(image_data)
-                            temp_file.close()
-                            #display image
-                            image = Image.open(temp_file.name)
-                            st.image(image)
-                        else:
-                            st.markdown(content)
+        # Display the assistant messages
+        function.display_assistant_message(st.session_state.messages)
+    
 
-        # Fetch and display stock information
-        st.subheader("Stock Information")
-        GetStockInformation = yahooFinance.Ticker(ticker)
-        data = ""
-        for key, value in GetStockInformation.info.items():
-            data += f"{key}: {value}\n"
-        st.text_area("Stock Data", data, height=500)
+        # Fetch and display stock information from Yahoo Finance
+        CompanyInfo = yahooFinance.Ticker(ticker)
+        function.display_company_info(CompanyInfo.info)
+        function.display_stock_info(CompanyInfo.info)
 
-        # Use OpenAI to analyze text (You need to handle the API call here)
-        # ...
-        # Show the result
 
 
 # Run the application
--- function.py
+++ function.py
@@ -5,6 +5,7 @@ import yfinance as yahooFinance
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+import time
 
 def scrape_http_links(url):
     # Send a request to the URL
@@ -83,6 +84,125 @@ def extract_content(text):
         script.decompose()
 
     paragraphs = soup.find_all('p')
+
     readable_text = '\n'.join([p.get_text() for p in paragraphs])
 
     return readable_text
+
+
+def display_assistant_message(message):
+    st.session_state.messages = message
+    for message in reversed(st.session_state.messages):
+            if message.role in ["assistant"]:
+                with st.chat_message(message.role):
+                    for content in message.content:
+                        if content.type == "text":
+                            st.markdown(content.text.value)
+                        elif content.type == "image_file":
+                            image_file = content.image_file.file_id
+                            image_data = client.files.content(image_file)
+                            image_data = image_data.read()
+                            #save image to temp file
+                            temp_file = tempfile.NamedTemporaryFile(delete=False)
+                            temp_file.write(image_data)
+                            temp_file.close()
+                            #display image
+                            image = Image.open(temp_file.name)
+                            st.image(image)
+                        else:
+                            st.markdown(content)
+
+# Chatgpt handler
+def gpt_handler(assistance_ID, processed_text, client, type="news"):
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=processed_text
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistance_ID,
+        instructions="Sanitize all the unreadable data and put it into bullet points"
+    )
+
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+    
+    st.session_state.run = run
+    pending = False
+    while st.session_state.run.status != "completed":
+        if not pending:
+            with st.chat_message("assistant"):
+                if type != "general":
+                    st.markdown("Processing...")
+            pending = True
+        time.sleep(3)
+        st.session_state.run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        
+            
+    if st.session_state.run.status == "completed": 
+        st.empty()
+    
+    st.session_state.messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        ).data
+    return st.session_state.messages
+
+
+
+
+
+
+
+
+
+# Function to display company information
+def display_company_info(info):
+    st.subheader("Company Information")
+    st.write(f"**Address:** {info['address1']}, {info['city']}, {info['zip']}, {info['country']}")
+    st.write(f"**Phone:** {info['phone']}")
+    st.write(f"**Website:** [Link]({info['website']})")
+    st.write(f"**Industry:** {info['industryDisp']}")
+    st.write(f"**Sector:** {info['sectorDisp']}")
+    st.write("### Business Summary")
+    st.write(info['longBusinessSummary'])
+
+# Function to display stock information
+def display_stock_info(info):
+    st.subheader("Stock Information")
+    st.write(f"**Previous Close:** {info['previousClose']}")
+    st.write(f"**Open:** {info['open']}")
+    st.write(f"**Day Low:** {info['dayLow']}")
+    st.write(f"**Day High:** {info['dayHigh']}")
+    st.write(f"**Volume:** {info['volume']}")
+    # Add more fields as needed
+
+# # Your stock data
+# stock_data = {
+#     "address1": "Building 20",
+#     "address2": "No. 56 AnTuo Road Anting Town Jiading District",
+#     "city": "Shanghai",
+#     "zip": "201804",
+#     "country": "China",
+#     "phone": "86 21 6908 2018",
+#     "website": "https://www.nio.com",
+#     "industry": "Auto Manufacturers",
+#     "industryDisp": "Auto Manufacturers",
+#     "sector": "Consumer Cyclical",
+#     "sectorDisp": "Consumer Cyclical",
+#     "longBusinessSummary": "NIO Inc. designs, develops, manufactures, and sells smart electric vehicles in China...",
+#     # ... add the rest of your data here
+#     "previousClose": 7.4,
+#     "open": 7.26,
+#     "dayLow": 7.16,
+#     "dayHigh": 7.53,
+#     "volume": 49034582
+#     # ... and so on
+# }
